5d857cc475cf51ce1eeff88ee8027adf58e0d682
1521498437 <1521498437@qq.com>
2020-04-27 06:42:11 +0800

    First commit

---
+++ 实验三：安卓UI组件/源码/java/com/fjnu/ydy/uicomponent/CustomDialog.java
@@ -0,0 +1,46 @@
+package com.fjnu.ydy.uicomponent;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+public class CustomDialog extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_dialog_tutorial);
+        Button btn_custom_dialog = (Button)findViewById(R.id.custom_dialog_btn);
+        btn_custom_dialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createDialog();
+            }
+        });
+    }
+
+    public void createDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.custom_dialog, null))
+
+                .setPositiveButton(R.string.signin, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
+        builder.create();
+        builder.show();
+    }
+
+}
